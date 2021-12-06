20f438760e78831869e0c3eb4bbc2787ecbd6704
EvanReilhac <91424557+EvanReilhac@users.noreply.github.com>
2021-12-07 00:19:31 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -34,27 +34,7 @@ int main() {
             tabChance = initCarteChance();
             melangerCartesChance(tabChance);
             melangerCartesCommunaute(tabCommu);
-
-            tabJoueur[0].possessionParCase[1] = 0;
-            tabJoueur[0].possessionParCase[3] = -1;
-            tabJoueur[0].possessionParCase[5] = -1;
-            tabJoueur[0].possessionParCase[7] = 0;
-            tabJoueur[0].possessionParCase[9] = 1;
-            tabJoueur[0].possessionParCase[11] = 2;
-            tabJoueur[0].possessionParCase[13] = 5;
-            tabJoueur[0].possessionParCase[15] = 0;
-            tabJoueur[0].possessionParCase[17] = 0;
-            tabJoueur[1].possessionParCase[19] = 0;
-            tabJoueur[1].possessionParCase[21] = 3;
-            tabJoueur[1].possessionParCase[23] = 0;
-            tabJoueur[1].possessionParCase[25] = 4;
-            tabJoueur[1].possessionParCase[27] = 5;
-            tabJoueur[1].possessionParCase[29] = 1;
-            tabJoueur[1].possessionParCase[31] = 1;
-            tabJoueur[0].nbProprietes = 8;
-            tabJoueur[1].nbProprietes = 8;
-            /*recapPossessionJoueur(tabJoueur[0], tabCase);
-            recapPossessionJoueur(tabJoueur[1], tabCase);*/
+            
             int jDebut = getRandomInteger1(nbJ);
             printf("La partie peut commencer, choisi avec le plus grand des hasard ! %s lance les hostilites !\n\n",
                    tabJoueur[jDebut - 1].nomJ);
@@ -214,37 +194,6 @@ int main() {
 
     }
 
-    /* Case *tabCase = initCases();
 
-     int nb = nombreJoueur();
-     Joueurs *tabJoueur = NULL;
-     tabJoueur = initJoueur(nb);
-     tabJoueur[0].possessionParCase[1] = 0;
-     tabJoueur[0].possessionParCase[3] = -1;
-     tabJoueur[0].possessionParCase[5] = -1;
-     tabJoueur[0].possessionParCase[7] = 0;
-     tabJoueur[0].possessionParCase[9] = 1;
-     tabJoueur[0].possessionParCase[11] = 2;
-     tabJoueur[0].possessionParCase[13] = 5;
-     tabJoueur[0].possessionParCase[15] = 0;
-     tabJoueur[0].possessionParCase[17] = 0;
-     tabJoueur[1].possessionParCase[19] = 0;
-     tabJoueur[1].possessionParCase[21] = 3;
-     tabJoueur[1].possessionParCase[23] = 0;
-     tabJoueur[1].possessionParCase[25] = 4;
-     tabJoueur[1].possessionParCase[27] = 5;
-     tabJoueur[1].possessionParCase[29] = 1;
-     tabJoueur[1].possessionParCase[31] = 1;
-     RecapPossessionJoueur(tabJoueur[0], tabCase);
-     RecapPossessionJoueur(tabJoueur[1], tabCase);
-     for (int i = 0; i < nb; i++) {
-         printf(" affichage relation par case de %s : ", tabJoueur[i].nomJ);
-         for (int j = 0; j < NOMBRE_CASE; j++) {
-             printf("%d \t ", tabJoueur[i].possessionParCase[j]);
-             printf("/");
-         }
-     }
-
- */
     return 0;
 }
