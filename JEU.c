54dfad99acd51b30945c0baedc53dc1f74c73cb0
Romainb9 <91485862+Romainb9@users.noreply.github.com>
2021-12-06 09:39:36 +0100

    Add files via upload

---
+++ JEU.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "MACRO.h"
+#include "EXO.h"
+#include "FONCTION.h"
+#include "JEU.h"
+
+
+/*void menuPrinc(){
+    srand(time(NULL));
+    int choix = 0;
+    int nbJoueurs = 0;
+    Joueurs** tablNom = NULL;
+    int taille = 0;
+    char* nom;
+    printf("Veuillez choisir parmis l'ensemble des options suivantes:\n1)Lancer une nouvelle partie.\n2)Sauvegarder la partie en cours.\n3)Charger une ancienne partie.\n4)Afficher les noms de l'equipe du projet.\n5)Afficher les regles du jeu.\n6)Quitter ce jeu.\n>");
+    scanf("%d", &choix);
+    switch (choix) {
+        case 1:
+            initCases();
+            nbJoueurs = nombreJoueur();
+            initJoueur(nbJoueurs);
+            //fonction random JM NN
+            //fonction jeu NN
+              //boucle for commenceant aux 1er joueur tiré NN
+                 //lancé vos dés OUI
+                 //fonction placement OUI
+                 //fonction qui récapitule l'ensembles des caractéristiques du joueur NN EVAN
+                 //fonction type NN EC
+                    //execution des différents case JM->cartes chances et commu NN/
+                    //fonction prison OUI
+                 //si double   OUi
+                    //lancé vos dés
+                    //fonction placement
+                    //fonction qui récapitule l'ensembles des caractéristiques du joueur
+                    //fonction type
+                     //execution des différents case JM->cartes chances et commu/
+                     //fonction prison
+            //si double
+                //lancé vos dés
+                //fonction placement
+                //fonction qui récapitule l'ensembles des caractéristiques du joueur
+                //fonction type
+                    //execution des différents case JM->cartes chances et commu/
+                    //fonction prison
+               //go prison
+        // fonction qui annonce le joueur suivant   NN
+        //fonction faillite NN PIERRE
+        //fonction hypotèque NN ROMAIN
+
+
+
+
+
+
+
+
+            break;
+        case 2:
+            //Sauvegarder partie en cours
+            break;
+        case 3:
+            //charger une ancienne partie
+            break;
+        case 4:
+            printf("Les devellopeurs de ce jeu sont:\n>Evan RHEILAC-AUPRETRE\n>Pierre ALLOUCHERIE\n>Jean-Marc COLLEGIA\n>Romain BOUCHER");
+            break;
+        case 5:
+            //règles du jeu
+            break;
+        case 6:
+            //quitter le jeu
+            break;
+
+    }
+}*/
---
+++ JEU.h
@@ -0,0 +1,8 @@
+//
+// Created by francois on 03/12/2021.
+//
+
+#ifndef MONOPOLY_JEU_H
+#define MONOPOLY_JEU_H
+
+#endif //MONOPOLY_JEU_H
